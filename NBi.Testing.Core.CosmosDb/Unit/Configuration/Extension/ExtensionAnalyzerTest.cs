b6e21772c2c830005c23280e25a69aed5d44a46f
Cédric L. Charlier <cedric.charlier@yucom.be>
2018-01-14 17:55:46 +0100

    Initial commit of code

---
+++ NBi.Testing.Core.CosmosDb/Unit/Configuration/Extension/ExtensionAnalyzerTest.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NBi.Core.Configuration.Extension;
+using NBi.Core.CosmosDb.Query.Execution;
+using NBi.Core.CosmosDb.Query.Command;
+using NBi.Core.CosmosDb.Query.Client;
+
+namespace NBi.Testing.Core.CosmosDb.Unit.Configuration.Extension
+{
+    public class ExtensionAnalyzerTest
+    {
+        [Test]
+        public void Execute_CosmosDbGraph_Three()
+        {
+            var analyzer = new ExtensionAnalyzer();
+            var types = analyzer.Execute("NBi.Core.CosmosDb");
+            Assert.That(types.Count(), Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Execute_CosmosDbGraph_ISessionFactory()
+        {
+            var analyzer = new ExtensionAnalyzer();
+            var types = analyzer.Execute("NBi.Core.CosmosDb");
+            Assert.That(types, Has.Member(typeof(GraphClientFactory)));
+        }
+
+        [Test]
+        public void Execute_CosmosDbGraph_ICommandFactory()
+        {
+            var analyzer = new ExtensionAnalyzer();
+            var types = analyzer.Execute("NBi.Core.CosmosDb");
+            Assert.That(types, Has.Member(typeof(GraphCommandFactory)));
+        }
+
+        [Test]
+        public void Execute_CosmosDbGraph_IExecutionEngine()
+        {
+            var analyzer = new ExtensionAnalyzer();
+            var types = analyzer.Execute("NBi.Core.CosmosDb");
+            Assert.That(types, Has.Member(typeof(GraphExecutionEngine)));
+        }
+    }
+}
