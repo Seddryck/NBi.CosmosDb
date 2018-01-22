785056e21d55ad4a71562ebf99c66cf8eed7f8ed
Cédric L. Charlier <cedric.charlier@yucom.be>
2018-01-22 16:03:44 +0100

    Fix these tests

--- NBi.Testing.Core.CosmosDb/Integration/NativeAPI.cs
+++ NBi.Testing.Core.CosmosDb/Integration/NativeAPI.cs
@@ -17,7 +17,7 @@ namespace NBi.Testing.Core.CosmosDb.Integration
         public void SqlApi()
         {
             var client = new DocumentClient(new Uri("https://localhost:8081/"), "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==");
-            var collection = client.ReadDocumentCollectionAsync(UriFactory.CreateDocumentCollectionUri("NBi.GraphDB", "FoF")).Result.Resource;
+            var collection = client.ReadDocumentCollectionAsync(UriFactory.CreateDocumentCollectionUri("NBi-GraphDB", "FoF")).Result.Resource;
             var queryable = client.CreateDocumentQuery<dynamic>(collection.SelfLink, "SELECT VALUE p.age[0]._value FROM ROOT p WHERE p.label = 'person' and p.firstName[0]._value = 'Mary'");
             var query = queryable.AsDocumentQuery();
             Assert.That(query.HasMoreResults);
@@ -30,7 +30,7 @@ namespace NBi.Testing.Core.CosmosDb.Integration
         public void GrapApi()
         {
             var client = new DocumentClient(new Uri("https://localhost:8081/"), "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==");
-            var collection = client.ReadDocumentCollectionAsync(UriFactory.CreateDocumentCollectionUri("NBi.GraphDB", "FoF")).Result.Resource;
+            var collection = client.ReadDocumentCollectionAsync(UriFactory.CreateDocumentCollectionUri("NBi-GraphDB", "FoF")).Result.Resource;
             var query = client.CreateGremlinQuery<dynamic>(collection, "g.V().has('firstName', 'Mary').values('age').Limit(1)");
             Assert.That(query.HasMoreResults);
             var res = query.ExecuteNextAsync().Result;
